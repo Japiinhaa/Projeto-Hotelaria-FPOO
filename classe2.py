bb5bd3fa8ced4cad93c59092248e60e69acbf769
Japiinhaa <vitorgoncalves964@gmail.com>
2023-08-24 14:18:10 -0300

    Update4

--- classe2.py
+++ classe2.py
@@ -1,26 +1,45 @@
 clientes = {}
+chave = 0
+chave_disponivel = 1
+
 cliente = []
 quartos = ['1- Apartamento simples', '2- Apartamento simples casal', '3- Apartamento simples casal', '4- Apartamento duplo', '5- Apartamento simples', '6- Apartamento duplo casal', '7- Apartamento duplo casal','8- Apartamento duplo', '9- Apartamento_master', '10- Apartamento_master', '11- Apartamento_Luxo', '12- Apartamento_Luxo' ]
 import os
 
 
 
+
 class Hotel():
-    def __init__(self):
+    def __init__(self, nome, cpf, quarto):
+        self.nome = []
+        self.cpf = []
+        self.quarto = []
         
     def pesquisar_quarto():
         seleçao_ = input ("")
 
+    def set_cliente():
 
-
-    def reservar():
-        cliente_nome = input ("Nome do cliente: ")
-        reserva = 
+        nome = input('Digite seu nome: ')
+        cpf = input('Digite o seu CPF: ')
+        quarto = 0
         
-        print 
+        if chave == chave_disponivel:
+            cliente = [nome, cpf, quarto]
+            clientes.append(cliente)
+            print('Apartamento reservado com sucesso!')
+        os.system('pause')
+        os.system('cls')
     
 
 class Quartos(Hotel):
+    def __init__(self, capacidade, preco, itens, tamanho):
+        self.capacidade = []
+        self.preco = []
+        self.itens = []
+        self.tamanho = []
+        
+
     def listar_especificações():
         print ('Capacidade: {capacidade}')
 
@@ -28,17 +47,6 @@ class Quartos(Hotel):
 
     def listar_quartos():
 
-    def set_cliente():
-
-        nome = input('Digite seu nome: ')
-        cpf = input('Digite o seu CPF: ')
-        quarto = 0
-        if quarto == 
-        cliente = [nome, cpf, quarto]
-        clientes.append(cliente)
-        print('Cliente cadastrado com sucesso!')
-        os.system('pause')
-        os.system('cls')
     
 
 
@@ -64,13 +72,13 @@ class AP_simples(Quartos):
     def especificações():
         capacidade = "2"
         preço = "350"
-        itens = 'Cama king size \nTV 8k 55" \nVista direta pro mar \nDuas vagas para garagem \nArea gourmet \nDuas suites'
+        itens = 'uma beliche \nTV 1080 40" \nVista direta pro mar \nDuas vagas para garagem \nArea gourmet \nDuas suites'
         tamanho = '50m²'   
 
 
 class AP_simples_casal(Quartos):
     def especificações():
-        capacidade = "2"
+        capacidade = "4"
         preço = "500"
         itens = 'Cama king size \nTV 8k 55" \nVista direta pro mar \nDuas vagas para garagem \nArea gourmet \nDuas suites'
         tamanho = '90m²'
@@ -78,14 +86,14 @@ class AP_simples_casal(Quartos):
 
 class AP_duplo(Quartos):
     def especificações():
-        capacidade = "2"
-        preço = "4085"
+        capacidade = "4"
+        preço = "680"
         itens = 'Cama king size \nTV 8k 55" \nVista direta pro mar \nDuas vagas para garagem \nArea gourmet \nDuas suites'
         tamanho = '70m²'
         
 class AP_duplo_casal(Quartos):
     def especificações():
         capacidade = "2"
-        preço = "4085"
+        preço = "800"
         itens = 'Cama king size \nTV 8k 55" \nVista direta pro mar \nDuas vagas para garagem \nArea gourmet \nDuas suites'
         tamanho = '111m²'
